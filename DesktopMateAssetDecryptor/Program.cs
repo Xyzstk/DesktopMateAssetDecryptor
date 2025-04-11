19d84cd41fa4bcd3a5fce15774cd465857c986a9
Xyzstk <13613846017@163.com>
2025-04-12 00:24:41 +0800

    Update for 1.2.0

--- DesktopMateAssetDecryptor/Program.cs
+++ DesktopMateAssetDecryptor/Program.cs
@@ -5,6 +5,18 @@ using System.Text;
 public class Decryptor
 {
     private const string keyFileName = "xriGM9Vwo1bXOkMVQPsnTw";
+    private const string saltString = "uAME672YZx4q8tJ11rA2hVGaNTF1w3vEvF+lY8cvyTIroViqxgzoZXTgkFnylMXa";
+
+    enum AssetBundleType
+    {
+        None,
+        iltan,
+        miku,
+        snow_miku_2025,
+        zundamon,
+        Max
+    }
+
     public static void Main(string[] args)
     {
         if (args.Length < 2) {
@@ -15,6 +27,16 @@ public class Decryptor
         string assetName = args[1];
         bool isEncrypt = args.Length >= 3 && args[2] == "E";
 
+        AssetBundleType assetBundleType = AssetBundleType.None;
+        if (!Enum.TryParse(assetName, false, out assetBundleType) || !(assetBundleType > AssetBundleType.None && assetBundleType < AssetBundleType.Max))
+        {
+            Console.WriteLine("Invalid AssetBundle type. Choose one of the following:");
+            foreach (AssetBundleType type in Enum.GetValues(typeof(AssetBundleType)))
+                if (type != AssetBundleType.None && type != AssetBundleType.Max)
+                    Console.WriteLine(type.ToString());
+            return;
+        }
+
         try
         {
             Stream keyFileStream = File.OpenRead(basePath + "/" + keyFileName);
@@ -22,15 +44,15 @@ public class Decryptor
             string encryptedFile = args[0] + "/" + SimpleAes.AesEncrypt(assetName, keyFileStream);
 
             string password = Encoding.UTF8.GetString(getKeyFileBytes(keyFileStream, getKeyFileOffset(keyFileStream, assetName, 3), 32));
-            byte[] salt = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(assetName));
+            byte[] salt = Convert.FromBase64String(saltString);
             if (isEncrypt)
             {
-                SeekableAes aesStream = new SeekableAes(File.Create(encryptedFile), password, salt);
+                SeekableAes aesStream = new SeekableAes(File.Create(encryptedFile), password, salt, keyFileStream, (byte)assetBundleType);
                 File.OpenRead(originalFile).CopyTo(aesStream);
             }
             else
             {
-                SeekableAes aesStream = new SeekableAes(File.OpenRead(encryptedFile), password, salt);
+                SeekableAes aesStream = new SeekableAes(File.OpenRead(encryptedFile), password, salt, keyFileStream, (byte)assetBundleType);
                 aesStream.CopyTo(File.Create(originalFile));
             }
         }
@@ -94,6 +116,21 @@ public class SeekableAes : Stream
     private ICryptoTransform encryptor;
     public bool autoDisposeBaseStream;
 
+    private const string key_str = "expand 32-byte key";
+    private Stream KeyFileStream;
+    private byte assetIdx;
+
+    struct Key
+    {
+        public Key(ulong counterInit)
+        {
+            raw = new uint[8];
+            counter = counterInit;
+        }
+        public uint[] raw;
+        public ulong counter;
+    }
+
     public override bool CanRead
     {
         get
@@ -138,17 +175,118 @@ public class SeekableAes : Stream
         }
     }
 
-    public SeekableAes(Stream rawStream, string password, byte[] salt)
+    public SeekableAes(Stream rawStream, string password, byte[] salt, Stream keyFileStream, byte assetIdx)
     {
         baseStream = rawStream;
         autoDisposeBaseStream = true;
-        Rfc2898DeriveBytes rfc = new Rfc2898DeriveBytes(password, salt);
+        Rfc2898DeriveBytes rfc = new Rfc2898DeriveBytes(password, salt, 2, HashAlgorithmName.SHA512);
         aesData = new AesManaged();
         aesData.KeySize = 256;
         aesData.Mode = CipherMode.ECB;
         aesData.Padding = PaddingMode.None;
         aesData.Key = rfc.GetBytes(32);
         encryptor = aesData.CreateEncryptor();
+        KeyFileStream = keyFileStream;
+        this.assetIdx = assetIdx;
+    }
+
+    void ror_int128(uint[] value, int byte_cnt)
+    {
+        for (int i = 0; i < byte_cnt; i++)
+        {
+            uint tmp = value[0];
+            value[0] = value[1];
+            value[1] = value[2];
+            value[2] = value[3];
+            value[3] = tmp;
+        }
+    }
+
+    private void rotate_enc(uint[][] buf)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            buf[0][i] += buf[1][i];
+            buf[3][i] = BitOperations.RotateLeft(buf[3][i] ^ buf[0][i], 16);
+            buf[2][i] += buf[3][i];
+            buf[1][i] = BitOperations.RotateLeft(buf[1][i] ^ buf[2][i], 12);
+            buf[0][i] += buf[1][i];
+            buf[3][i] = BitOperations.RotateLeft(buf[3][i] ^ buf[0][i], 8);
+            buf[2][i] += buf[3][i];
+            buf[1][i] = BitOperations.RotateLeft(buf[1][i] ^ buf[2][i], 7);
+        }
+    }
+
+    private byte[] parse_key(ref Key key, int loopCnt)
+    {
+        byte[] res = new byte[0x100];
+        uint[] strkey = new uint[4];
+        Buffer.BlockCopy(Encoding.UTF8.GetBytes(key_str), 0, strkey, 0, 16);
+        uint[][] buf = new uint[4][];
+        for (int i = 0; i < 4; i++)
+            buf[i] = new uint[4];
+        for (int i = 0; i < 4; i++)
+        {
+            uint[] parsed = new uint[16];
+            for (int j = 0; j < 4; j++)
+            {
+                parsed[j] = buf[0][j] = strkey[j];
+                parsed[j + 4] = buf[1][j] = key.raw[j];
+                parsed[j + 8] = buf[2][j] = key.raw[j + 4];
+                parsed[j + 12] = buf[3][j] = (uint)((UInt128)key.counter >> (32 * j));
+            }
+            for (int j = 0; j < loopCnt; j++)
+            {
+                rotate_enc(buf);
+                ror_int128(buf[1], 1);
+                ror_int128(buf[2], 2);
+                ror_int128(buf[3], 3);
+                rotate_enc(buf);
+                ror_int128(buf[1], 3);
+                ror_int128(buf[2], 2);
+                ror_int128(buf[3], 1);
+            }
+            for (int j = 0; j < 4; j++)
+                for (int k = 0; k < 4; k++)
+                    parsed[j * 4 + k] += buf[j][k];
+            Buffer.BlockCopy(parsed, 0, res, i * 64, 64);
+            key.counter++;
+        }
+        return res;
+    }
+
+    private void crypto(byte[] buffer, int count, long streamPos)
+    {
+        Key key = new Key(0);
+        ulong value = assetIdx + 0x676A3E970301B49Du;
+        for (int i = 0; i < 8; i++)
+        {
+            value = 0x5851F42D4C957F2Du * value - 0x5E89AB1B9041E80Du;
+            key.raw[i] = BitOperations.RotateRight((uint)((value >> 27) ^ (value >> 45)), (int)(value >> 59));
+        }
+        byte[] parsed_key = parse_key(ref key, 4);
+        byte[] keyBlock = new byte[0x7F7];
+        UInt128 filePos = (UInt128)BitConverter.ToUInt64(parsed_key, 0) * (UInt128)(KeyFileStream.Length - 0x7F7);
+        if (0x7F7u - (ulong)KeyFileStream.Length < (ulong)filePos)
+            filePos += ((UInt128)BitConverter.ToUInt64(parsed_key, 8) * (UInt128)(KeyFileStream.Length - 0x7F7)) >> 64;
+        KeyFileStream.Seek((long)(filePos >> 64), SeekOrigin.Begin);
+        KeyFileStream.Read(keyBlock, 0, 0x7F7);
+        key.counter = (ulong)streamPos / 0x650;
+        parsed_key = parse_key(ref key, 4);
+        long keyPos = (streamPos / 101) & 0xF;
+        for (int i = 0; i < count; i++, streamPos++)
+        {
+            buffer[i] ^= parsed_key[keyPos * 4];
+            buffer[i] ^= keyBlock[streamPos % 0x7F7];
+            if (streamPos % 101 == 100)
+            {
+                if (++keyPos >= 0x40)
+                {
+                    parsed_key = parse_key(ref key, 4);
+                    keyPos = 0;
+                }
+            }
+        }
     }
 
     private void cipher(byte[] buffer, int offset, int count, long streamPos)
@@ -162,7 +300,7 @@ public class SeekableAes : Stream
         while (offset < count) {
             if(!flag || blockoff % size == 0)
             {
-                System.BitConverter.GetBytes(blockidx).CopyTo(srcblock, 0);
+                BitConverter.GetBytes(blockidx).CopyTo(srcblock, 0);
                 encryptor.TransformBlock(srcblock, 0, size, dstblock, 0);
                 blockidx++;
                 if(flag) blockoff = 0;
@@ -192,11 +330,13 @@ public class SeekableAes : Stream
         var pos = Position;
         int res = baseStream.Read(buffer, offset, count);
         cipher(buffer, offset, count, pos);
+        crypto(buffer, count, pos);
         return res;
     }
 
     public override void Write(byte[] buffer, int offset, int count)
     {
+        crypto(buffer, count, Position);
         cipher(buffer, offset, count, Position);
         baseStream.Write(buffer, offset, count);
     }
---
+++ MetadataDecryptor/Program.cs
@@ -0,0 +1,128 @@
+﻿public class MetadataDecryptor
+{
+    private const int headerSize = 0x150;
+    private const byte headerKey = 0x50;
+    struct BlockInfo
+    {
+        public BlockInfo(int _valEntry, int _sizeEntry, int _offset1, int _offset2, bool _sign)
+        {
+            valEntry = _valEntry;
+            sizeEntry = _sizeEntry;
+            offset1 = _offset1;
+            offset2 = _offset2;
+            sign = _sign;
+        }
+        public int valEntry;
+        public int sizeEntry;
+        public int offset1;
+        public int offset2;
+        public bool sign;
+    }
+    private static readonly BlockInfo[] encryptedBlocks = {
+        new BlockInfo(0x38, 0xC0, -0x1C, -0x20, false),
+        new BlockInfo(0x14, 0x104, -0x3C, -0x20, false),
+        new BlockInfo(0x114, 0x34, -0x14, 0x20, true),
+        new BlockInfo(0x8, 0xD0, -0x28, 0x20, true),
+        new BlockInfo(0xFC, 0xF8, 0x24, -0x20, false),
+        new BlockInfo(0x28, 0x18, -0x30, 0x20, true),
+        new BlockInfo(0x10, 0, 0x18, 0x20, true)
+    };
+    private static readonly (int, int)[] vectorOffset =
+    {
+        (0x30, -0x28),
+        (0x60, -0x18),
+        (0x3C, -0x14),
+        (0xD4, -0x34),
+        (0xAC, -0x14),
+        (0x68, -0x38),
+        (0x48, -0x30),
+        (0x12C, 0x1C),
+        (0x9C, -0x18),
+        (0xF4, 0x38),
+        (0x20, -0x3C),
+        (0x58, 0x3C),
+        (0xB0, 0x24),
+        (0xDC, -0x28),
+        (0x11C, 0x28),
+        (0xC8, 0x20),
+        (0x13C, 0x1C),
+        (0xBC, 0x20),
+        (0x8C, -0x30),
+        (0x38, -0x1C),
+        (0x14, -0x3C),
+        (0x114, -0x14),
+        (0x8, -0x28),
+        (0xFC, 0x24),
+        (0x28, -0x30),
+        (0x10, 0x18),
+        (0x44, -0x38),
+        (0x54, 0x10),
+        (0xE8, 0x20),
+        (0x140, -0x24),
+        (0x10C, 0x34),
+    };
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2) return;
+        try
+        {
+            Stream fileStream = File.Create(args[1]);
+            Stream encryptedFileStream = File.OpenRead(args[0]);
+            // Decrypt header
+            byte[] headerRaw = new byte[headerSize];
+            encryptedFileStream.Seek(0, SeekOrigin.Begin);
+            encryptedFileStream.Read(headerRaw, 0, headerSize);
+            for(int i = 0; i < headerSize; i++)
+            {
+                headerRaw[i] ^= (byte)(headerKey - (i & 0xFF));
+            }
+            int[] header = new int[headerSize >> 2];
+            for (int i = 0; i < header.Length; i++)
+                header[i] = BitConverter.ToInt32(headerRaw, i * 4);
+            // Reserve space for the header
+            fileStream.Seek(0x100, SeekOrigin.Begin);
+            encryptedFileStream.CopyTo(fileStream);
+            encryptedFileStream.Close();
+            // Decrypt blocks
+            for(int i = 0; i < encryptedBlocks.Length; i++)
+            {
+                BlockInfo block = encryptedBlocks[i];
+                decryptBlock(fileStream, header[block.valEntry >> 2], header[block.sizeEntry >> 2], block.offset1, block.offset2, block.sign);
+            }
+            // Rebuild header
+            int[] blockVectors = vectorOffset.Select(v => header[v.Item1 >> 2] + v.Item2 + 0x100 - headerSize).ToArray();
+            Array.Sort(blockVectors);
+            int[] newHeader = new int[64];
+            newHeader[0] = unchecked((int)0xFAB11BAF);
+            newHeader[1] = 29;
+            for (int i = 0; i < blockVectors.Length; i++)
+            {
+                newHeader[i * 2 + 2] = blockVectors[i];
+                newHeader[i * 2 + 3] = (i == blockVectors.Length - 1 ? (int)fileStream.Length : blockVectors[i + 1]) - blockVectors[i];
+            }
+            byte[] newHeaderRaw = new byte[0x100];
+            Buffer.BlockCopy(newHeader, 0, newHeaderRaw, 0, 0x100);
+            fileStream.Seek(0, SeekOrigin.Begin);
+            fileStream.Write(newHeaderRaw);
+            fileStream.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Operation failed:\n" + e.ToString());
+        }
+    }
+
+    public static void decryptBlock(Stream fileStream, int addr, int size, int offset1, int offset2, bool sign)
+    {
+        byte[] buf = new byte[size];
+        int fileOffset = addr + offset1 + 0x100 - headerSize;
+        fileStream.Seek(fileOffset, SeekOrigin.Begin);
+        fileStream.Read(buf, 0, size);
+        for (int i = 0; i < size; i++)
+        {
+            buf[i] ^= (byte)(size * (sign ? -1 : 1) + i + offset2);
+        }
+        fileStream.Seek(fileOffset, SeekOrigin.Begin);
+        fileStream.Write(buf, 0, size);
+    }
+}
